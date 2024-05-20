68cff807db45eb7fcbe8b06270e9ed84b3938b9d
SlavikL <comp.slavik@gmail.com>
2024-05-20 22:35:31 +0400

    singleton add

---
+++ src/main/java/com/designpatterns2/DesignPatterns2Application.java
@@ -0,0 +1,12 @@
+package com.designpatterns2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatterns2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatterns2Application.class, args);
+	}
+}
